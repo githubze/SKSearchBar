e5a0483d6a96882d01e5bea63623eee5a5761d9e
阿汤哥 <1424395628@qq.com>
2019-05-30 14:45:49 +0800

    no message

--- SKSearchBar.h
+++ SKSearchBar.h
@@ -7,10 +7,100 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SKDefine.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
+@protocol SKSearchBarDelegete<NSObject>
+
+/**
+ 开始编辑
+ */
+- (void)sk_searchBarTextDidBeginEditing:(UISearchBar *)searchBar;
+
+/**
+ 结束编辑
+ */
+- (void)sk_searchBarTextDidEndEditing:(UISearchBar *)searchBar;
+
+/**
+ 搜索按钮
+ */
+- (void)sk_searchBarSearchButtonClicked:(UISearchBar *)searchBar;
+
+/**
+ 取消按钮
+ */
+- (void)sk_searchBarCancelButtonClicked:(UISearchBar *)searchBar;
+
+@end
+
+typedef NS_ENUM(NSInteger,SKSearchAlignmentType){
+    SKSearchAlignmentTypeLeft,
+    SKSearchAlignmentTypeCenter
+};
+
 @interface SKSearchBar : UISearchBar
+
+/**
+ 协议
+ */
+@property (nonatomic, weak) id<SKSearchBarDelegete> sk_delegate;
+
+/**
+ 搜索区域背景图片
+ */
+@property (nonatomic, strong) UIImage * searchFieldBackgroundImage;
+
+/**
+ 搜索图片
+ */
+@property (nonatomic, strong) UIImage * searchBarIcon;
+
+/**
+ 搜索文字颜色
+ */
+@property (nonatomic, strong) UIColor * searchTextColor;
+
+/**
+ 搜索PlaceHolder颜色
+ */
+@property (nonatomic, strong) UIColor * searchPlaceHolderColor;
+
+/**
+ 搜索文字字体
+ */
+@property (nonatomic, strong) UIFont * searchTextFont;
+
+/**
+ 是否显示取消按钮 默认显示
+ */
+@property (nonatomic, assign) BOOL showCancelButton;
+
+/**
+ 取消按钮文字
+ */
+@property (nonatomic, strong) NSString * cancelButtonTitle;
+
+/**
+ 取消按钮文字颜色
+ */
+@property (nonatomic, strong) UIColor * cancelButtonTextColor;
+
+/**
+ 取消按钮字体
+ */
+@property (nonatomic, strong) UIFont * cancelButtonFont;
+
+/**
+ 对齐方式
+ */
+@property (nonatomic, assign) SKSearchAlignmentType searchAlignmentType;
+
+/**
+ 是否是编辑状态
+ */
+@property (nonatomic, assign) BOOL isEdting;
 
 @end
 
--- SKSearchBar.m
+++ SKSearchBar.m
@@ -8,7 +8,7 @@
 
 #import "SKSearchBar.h"
 
-@interface SKSearchBar () <UITextFieldDelegate>
+@interface SKSearchBar () <UITextFieldDelegate,UISearchBarDelegate>
 
 // placeholder 和icon 和 间隙的整体宽度
 @property (nonatomic, assign) CGFloat placeholderWidth;
@@ -18,67 +18,133 @@
 // icon宽度
 static CGFloat const searchIconW = 15.0;
 // icon与placeholder间距
-static CGFloat const iconSpacing = 10.0;
+static CGFloat const iconSpacing = 6.0;
+// 取消按钮宽
+static CGFloat const cannelSpacing = 32.0;
 
 @implementation SKSearchBar
 
 - (instancetype)initWithFrame:(CGRect)frame{
     if (self = [super initWithFrame:frame]) {
-        self.barTintColor = KTextColorfff;
+        
+        [self initData];
     }
     return self;
+}
+
+- (void)initData{
+    self.barTintColor = KTextColorfff;
+    self.searchBarIcon = kimage(@"searchIcon");
+    self.searchFieldBackgroundImage = kimage(@"SearchBarBg");
+    self.showCancelButton = YES;
+    self.cancelButtonTitle = @"取消";
+    self.cancelButtonTextColor = KTextColor333;
+    self.cancelButtonFont = kFontFix(14);
+    self.placeholder = @"请输入关键字";
+    self.searchTextFont = kFontFix(14);
+    self.searchTextColor = KTextColor333;
+    self.searchPlaceHolderColor = UIColorFromRGB(0xA1A1A1);
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
     
-    [self setSearchFieldBackgroundImage:kimage(@"SearchBarBg") forState:UIControlStateNormal];
-    [self setImage:kimage(@"searchIcon") forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
+    [self setSearchFieldBackgroundImage:self.searchFieldBackgroundImage forState:UIControlStateNormal];
+    [self setImage:self.searchBarIcon forSearchBarIcon:UISearchBarIconSearch state:UIControlStateNormal];
+    
     self.layer.borderWidth = 1;
     self.layer.borderColor = [[UIColor whiteColor] CGColor];
     self.searchTextPositionAdjustment = UIOffsetMake(iconSpacing,0);
-
-    UITextField * searchField = [self valueForKey:@"_searchField"];
-    searchField.textColor = KTextColor333;
-    searchField.font = kFontFix(12);
-    [searchField setValue:KTextColor999 forKeyPath:@"_placeholderLabel.textColor"];
-    searchField.frame = CGRectMake(15, (self.frame.size.height-32)/2, self.frame.size.width-30, 32);
+    self.delegate = self;
     
-    if (@available(iOS 11.0, *)) {
-        // 先默认居中placeholder
-        [self setPositionAdjustment:UIOffsetMake((searchField.frame.size.width-self.placeholderWidth)/2, 0) forSearchBarIcon:UISearchBarIconSearch];
+    for (UIView *view in [[self.subviews lastObject] subviews]) {
+        if ([view isKindOfClass:[UIButton class]]) {
+            UIButton *cancelBtn = (UIButton *)view;
+            [cancelBtn setTitle:self.cancelButtonTitle forState:UIControlStateNormal];
+            cancelBtn.titleLabel.font = self.cancelButtonFont;
+            [cancelBtn setTitleColor:self.cancelButtonTextColor forState:UIControlStateNormal];
+        }
+    }
+    
+    UITextField * searchField = [self valueForKey:@"_searchField"];
+    searchField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    searchField.textColor = self.searchTextColor;
+    searchField.font = self.searchTextFont;
+    [searchField setValue:self.searchPlaceHolderColor forKeyPath:@"_placeholderLabel.textColor"];
+    searchField.frame = CGRectMake(15, (self.frame.size.height-34)/2-1, self.frame.size.width-30-(self.showsCancelButton?cannelSpacing:0), 34);
+    
+    if (self.searchAlignmentType == SKSearchAlignmentTypeCenter &&self.isEdting==NO) {
+        if (@available(iOS 11.0, *)) {
+            // 先默认居中placeholder
+            [self setPositionAdjustment:UIOffsetMake((searchField.frame.size.width-self.placeholderWidth)/2, 0) forSearchBarIcon:UISearchBarIconSearch];
+        }
     }
 }
 
 // 计算placeholder、icon、icon和placeholder间距的总宽度
 - (CGFloat)placeholderWidth {
     if (!_placeholderWidth) {
-        CGSize size = [self.placeholder boundingRectWithSize:CGSizeMake(MAXFLOAT, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName:kFontFix(12)} context:nil].size;
-        _placeholderWidth = size.width + iconSpacing + searchIconW;
+        CGSize size = [self.placeholder boundingRectWithSize:CGSizeMake(MAXFLOAT, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin|NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName:kFontFix(14)} context:nil].size;
+        _placeholderWidth = size.width + iconSpacing + searchIconW+8;
     }
     return _placeholderWidth;
 }
 
-// 开始编辑的时候重置为靠左
-- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+#pragma mark - delegate
+
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar{
     // 继续传递代理方法
-    if ([self.delegate respondsToSelector:@selector(searchBarShouldBeginEditing:)]) {
-        [self.delegate searchBarShouldBeginEditing:self];
+    self.showsCancelButton = self.showCancelButton;
+    self.isEdting = YES;
+    if (self.showCancelButton) {
+        [self layoutSubviews];
     }
+    
+    if ([self.sk_delegate respondsToSelector:@selector(sk_searchBarTextDidBeginEditing:)]) {
+        [self.sk_delegate sk_searchBarTextDidBeginEditing:self];
+    }
+    
     if (@available(iOS 11.0, *)) {
         [self setPositionAdjustment:UIOffsetZero forSearchBarIcon:UISearchBarIconSearch];
     }
-    return YES;
-}
-// 结束编辑的时候设置为居中
-- (BOOL)textFieldShouldEndEditing:(UITextField *)textField {
-    if ([self.delegate respondsToSelector:@selector(searchBarShouldEndEditing:)]) {
-        [self.delegate searchBarShouldEndEditing:self];
-    }
-    if (@available(iOS 11.0, *)) {
-        [self setPositionAdjustment:UIOffsetMake((textField.frame.size.width-self.placeholderWidth)/2, 0) forSearchBarIcon:UISearchBarIconSearch];
-    }
-    return YES;
 }
 
+- (void)searchBarTextDidEndEditing:(UISearchBar *)searchBar{
+    self.showsCancelButton = NO;
+    self.isEdting = NO;
+    
+    if ([self.sk_delegate respondsToSelector:@selector(sk_searchBarTextDidEndEditing:)]) {
+        [self.sk_delegate sk_searchBarTextDidEndEditing:self];
+    }
+    
+}
+//- (void)textFieldDidBeginEditing:(UITextField *)textField{
+//
+//    if (self.showCancelButton) {
+//        CGRect frame = textField.frame;
+//        frame.size.width -= cannelSpacing;
+//        textField.frame = frame;
+//    }
+//}
+- (void)textFieldDidEndEditing:(UITextField *)textField{
+    if (self.searchAlignmentType == SKSearchAlignmentTypeCenter) {
+        if (@available(iOS 11.0, *)) {
+            
+            [self setPositionAdjustment:UIOffsetMake((textField.frame.size.width-self.placeholderWidth)/2, 0) forSearchBarIcon:UISearchBarIconSearch];
+        }
+    }
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar{
+    if ([self.sk_delegate respondsToSelector:@selector(sk_searchBarCancelButtonClicked:)]) {
+        [self.sk_delegate sk_searchBarCancelButtonClicked:self];
+    }
+    [self resignFirstResponder];
+}
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+    if ([self.sk_delegate respondsToSelector:@selector(sk_searchBarSearchButtonClicked:)]) {
+        [self.sk_delegate sk_searchBarSearchButtonClicked:self];
+    }
+    [self resignFirstResponder];
+}
 @end
